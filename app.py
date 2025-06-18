2f319ef931d66b028dce875ff01b03e73a3468b3
jleonardofs079 <jleonardofs079@outlook.com>
2025-06-18 17:12:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -137,18 +137,18 @@ empreendimento = st.selectbox("Selecione o Empreendimento", options=["Todos"] + 
 
 # Aplicar filtros
 df_filtrado = df.copy()
-if cidade != "Todas":
 if cidade:
     df_filtrado = df_filtrado[df_filtrado['CIDADE'].isin(cidade)]
-if bairro != "Todos":
+    df_filtrado = df_filtrado[df_filtrado["CIDADE"] == cidade]
 if bairro:
     df_filtrado = df_filtrado[df_filtrado['BAIRRO'].isin(bairro)]
-if construtora != "Todas":
+    df_filtrado = df_filtrado[df_filtrado["BAIRRO"] == bairro]
 if construtora:
     df_filtrado = df_filtrado[df_filtrado['CONSTRUTORA'].isin(construtora)]
-if empreendimento != "Todos":
+    df_filtrado = df_filtrado[df_filtrado["CONSTRUTORA"] == construtora]
 if empreendimento:
     df_filtrado = df_filtrado[df_filtrado['EMPREENDIMENTO'].isin(empreendimento)]
+    df_filtrado = df_filtrado[df_filtrado["EMPREENDIMENTO"] == empreendimento]
 
 # Exibir resultados sem LATITUDE e LONGITUDE
 df_exibicao = df_filtrado.drop(columns=["LATITUDE", "LONGITUDE"], errors="ignore").copy()
